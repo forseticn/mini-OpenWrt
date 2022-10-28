31f2bdb30590a7d79618f02f5f6b15465f5e64a7
forseticn <63159639+forseticn@users.noreply.github.com>
2022-10-28 12:35:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,6 +13,9 @@
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.0.3/g' package/base-files/files/bin/config_generate
 
+#Clear the login password
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+
 # 移除重复软件包
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/applications/luci-app-netdata
